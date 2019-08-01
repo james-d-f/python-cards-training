c3f0abaebee0d412259a67144cba255e8f709ae8
James Freeman <james.f@brainlabsdigital.com>
2019-08-01 12:57:10 +0100

    Create brainlabs.py

---
+++ brainlabs.py
@@ -0,0 +1,11 @@
+name: Python Card Training
+status: active
+meta:
+  ignore: true
+ownership:
+  owner: james.f
+  maintainers:
+    - james.f
+public-info:
+  reach: other 
+  release-date: 2019-05-23
